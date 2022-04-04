e4c2de6d5d8bda8eabca0c853aa1b793ba1c4233
Luis <jqluis2011@gmail.com>
2022-04-03 18:41:08 -0700

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+  function findMatching(drivers, carman) {
+    return drivers.filter(function (driver) { 
+        return driver.toLowerCase() === carman.toLowerCase() })
+  }
+
+  function fuzzyMatch(drivers,chofer) {
+    return drivers.filter(function (driver) { 
+      return driver.toLowerCase().substring(0, chofer.length) === chofer.toLowerCase() })
+  }
+
+  function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { 
+      return driver.name === argument })
+  }
